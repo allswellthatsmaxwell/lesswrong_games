e70b22cb66ac40c21b140ed7103fa54cf19984bd
Maxwell <allswellthatsmaxwell9@gmail.com>
2022-03-28 22:10:22 -0500

    calibration plot and deck defeater class

--- py/mtg.py
+++ py/mtg.py
@@ -1,9 +1,7 @@
-from codecs import ignore_errors
-import enum
 import pandas as pd, numpy as np
 from sklearn.model_selection import train_test_split
 from functools import cached_property
-import itertools
+import plotnine as pn
 from typing import Tuple, Dict, List
 import lightgbm as lgb
 from scipy.special import comb
@@ -130,4 +128,92 @@ class RivalDeck:
         return dat
         
 
+class DeckDefeater:
+    def __init__(self, model, possible_decks: pd.DataFrame, rival_deck: pd.DataFrame) -> None:
+        self.model = model
+        self.possible_decks = possible_decks
+        self.rival_deck = rival_deck
+        assert rival_deck.shape[0] == 1
+        assert len(model.classes_) == 2
+        self.positive_class_idx = np.argmax(model.classes_)
+
+    @property
+    def possible_games(self) -> pd.DataFrame:
+        self.possible_decks.columns = [c.replace('_Deck_A_Count', '') + '_Deck_A_Count' 
+                                       for c in self.possible_decks.columns]
+        self.rival_deck.columns = [c.replace('_Deck_B_Count', '') + '_Deck_B_Count' 
+                                   for c in self.rival_deck.columns]
+        possible_games = (
+            pd.merge(self.possible_decks.assign(k='k'), 
+                     self.rival_deck.assign(k='k'), on='k')
+            .drop('k', axis=1)
+            .astype(int))
+        return possible_games
+
+    @cached_property
+    def possible_decks_and_predictions(self):
+        possible_probas = self.model.predict_proba(self.possible_games)
+        return (
+            self.possible_games
+            .assign(p=possible_probas[:, self.positive_class_idx])
+            .sort_values('p', ascending=False)
+            [list(self.possible_decks.columns) + ['p']]
+            .reset_index()
+            .drop('index', axis=1))
+
+    @cached_property
+    def mean_win_proba(self):
+        return self.possible_decks_and_predictions['p'].mean()
+
+    @cached_property
+    def best_deck_str(self):
+        best_deck_against_rival = (
+            self.possible_decks_and_predictions
+            .iloc[0][self.possible_decks.columns]
+            .astype(int)
+            .reset_index())
+        best_deck_against_rival.columns = ['card', 'n']
+        return '\n'.join(best_deck_against_rival.apply(lambda r: f"{r['card']}: {r['n']}", axis=1))
+
     
+        
+
+class CalibrationAnalyzer:
+    def __init__(self, y, pred, bucket_span=5) -> None:
+        assert len(y) == len(pred)
+        self.y = y
+        self.pred = pred
+        self.bucket_span = bucket_span
+    
+    @cached_property
+    def dat(self):
+        return (pd.DataFrame({'y': self.y, 'pred': self.pred})
+                .assign(bucket=lambda d: (d['pred'] * 100 // self.bucket_span).astype(int)))
+
+    @cached_property
+    def buckets_dat(self):
+        return (
+            pd.merge(
+                self.dat.groupby('bucket').agg({'y': np.mean, 'pred': np.mean}).reset_index(),
+                self.dat.groupby('bucket').agg({'y': len}).reset_index(),
+                on='bucket')
+            .rename({'y_x': 'y_true', 'y_y': 'n'}, axis=1))
+
+    @property
+    def plot(self):
+        breaks = np.arange(0, 1.001, self.bucket_span / 100)
+        axis_kwargs = dict(breaks=breaks, limits=(0, 1),
+                           labels=lambda ar: [int(x*100) for x in ar])
+        return (
+            pn.ggplot(self.buckets_dat.assign(group='group'),
+                        pn.aes(y='y_true', x='pred', group='group')) +
+            pn.geom_point(color='red') +
+            pn.geom_line(color='blue') +
+            pn.geom_abline(slope=1, intercept=0) +
+            pn.scale_x_continuous(**axis_kwargs) +
+            pn.scale_y_continuous(**axis_kwargs) +
+            pn.theme_bw() +
+            pn.theme(figure_size=(5, 5), panel_grid_minor=pn.element_blank(),
+                     axis_text_y=pn.element_text(size=8), 
+                     axis_text_x=pn.element_text(angle=0, size=6.5))                     
+        )
